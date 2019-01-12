7be8bec3fd32a6eb03569815006bd7fbecfd508a
Jonathan DiQuattro <jondiquattro@hotmail.com>
2019-01-11 21:24:20 -0800

    Got a good start on testing

--- src/middleware/model-finder.js
+++ src/middleware/model-finder.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (req,res,next) => {
+module.exports = (req,res,next) => {//req.params contains whatever variable is entered into the request ie categories
     let modelName = req.params.model;
     // console.log(modelName);
     req.model = require(`../models/${modelName}.js`);
